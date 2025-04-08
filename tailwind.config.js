1467eeded390009d314ba92140f0c0d3292782d3
Jeffreyw7 <138037670+Jeffreyw7@users.noreply.github.com>
2025-04-08 12:02:08 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#471AA0",
+        secondary: {
+          DEFAULT: "#BB84E8",
+          100: "#BB84E8",
+          200: "#FF8E01",
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#1E1E2D",
+          200: "#232533",
+        },
+        gray: {
+          100: "#2E2E2E",
+        },
+        carecircle: {
+          purple: {
+            light: "#BB84E8",
+            dark: "#471AA0",
+          }
+        }
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
